4d9e98176be882f49560b85d2b6ca43cd6534aa9
EscapeManOZK <romowczarek@gmail.com>
2018-01-08 19:42:11 +0100

    interface graphique V1

---
+++ ProjetWeb/ProjetWeb/Controllers/RechercheController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetWeb.Controllers
+{
+    public class RechercheController : Controller
+    {
+        // GET: Recherche
+        public ActionResult Epoque()
+        {
+            return View();
+        }
+        public ActionResult Artiste()
+        {
+            return View();
+        }
+        public ActionResult Genre()
+        {
+            return View();
+        }
+        public ActionResult Oeuvre()
+        {
+            return View();
+        }
+        public ActionResult Instrument()
+        {
+            return View();
+        }
+
+    }
+}
