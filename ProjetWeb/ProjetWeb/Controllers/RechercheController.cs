dd640afcf5226a8da04d3e90a15f32503907d99a
Gabriel Razafy <gabrielrazafy@gmail.com>
2018-01-19 14:40:47 +0100

    Update Artiste+Album

--- ProjetWeb/ProjetWeb/Controllers/RechercheController.cs
+++ ProjetWeb/ProjetWeb/Controllers/RechercheController.cs
@@ -12,7 +12,8 @@ namespace ProjetWeb.Controllers
     public class RechercheController : Controller
     {
         private Classique_Web_2017Entities bd = new Classique_Web_2017Entities();
-        // GET: Recherche
+
+        
         public ActionResult Epoque()
         {
             var oeuvre = bd.Oeuvre.OrderBy(m => m.Annee).Where(m => m.Annee.Value>0);
@@ -23,6 +24,21 @@ namespace ProjetWeb.Controllers
             var musicien = bd.Musicien.Include(m => m.Genre).Include(m => m.Instrument).Include(m => m.Pays);
             return View(musicien.ToList());
         }
+
+        [Route("RechercheArtiste/{lettre}")]
+        public ActionResult RechercheArtiste(string lettre)
+        {
+            var musicien = bd.Musicien.Where(m => m.Nom_Musicien.StartsWith(lettre));
+            
+            if (musicien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musicien.ToList());
+
+        }
+
+
         public ActionResult Genre()
         {
             return View();
@@ -48,11 +64,58 @@ namespace ProjetWeb.Controllers
             }
             return View(oeuvre);
         }
+
+        public ActionResult DetailMusicien(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musicien musicien = bd.Musicien.Find(id);
+            if (musicien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musicien);
+        }
+
+
+        public ActionResult Album(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var album = (from d in bd.Album
+                         join disque in bd.Disque on d.Code_Album equals disque.Code_Album
+                         join CompositionDi in bd.Composition_Disque on disque.Code_Disque equals CompositionDi.Code_Disque
+                         join enreg in bd.Enregistrement on CompositionDi.Code_Morceau equals enreg.Code_Morceau
+                         join compo in bd.Composition on enreg.Code_Composition equals compo.Code_Composition
+                         join compoOeu in bd.Composition_Oeuvre on compo.Code_Composition equals compoOeu.Code_Composition
+                         join oeuvre in bd.Oeuvre on compoOeu.Code_Oeuvre equals oeuvre.Code_Oeuvre
+                         join composer in bd.Composer on oeuvre.Code_Oeuvre equals composer.Code_Oeuvre
+                         where (composer.Code_Musicien == id)select  d).Distinct();
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album.ToList());
+        }
+
+
         public ActionResult Photo(int id)
         {
             var music = bd.Musicien.Single(g => g.Code_Musicien == id);
             if (music.Photo != null)
                 return File(music.Photo, "image/jpeg");
+            else return null;
+        }
+
+        public ActionResult Pochette(int id)
+        {
+            var album = bd.Album.Single(g => g.Code_Album == id);
+            if (album.Pochette != null)
+                return File(album.Pochette, "image/jpeg");
             else return null;
         }
 
