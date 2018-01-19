8c778975865a878da9b834b107d45f0410f03598
Owczarek Romain <romain.owczarek@etu.u-bordeaux.fr>
2018-01-19 15:52:48 +0100

    df

--- ProjetWeb/ProjetWeb/Controllers/HomeController.cs
+++ ProjetWeb/ProjetWeb/Controllers/HomeController.cs
@@ -22,10 +22,31 @@ namespace ProjetWeb.Controllers
         public ActionResult Panier()
         {
             if (!Request.IsAuthenticated) { 
-                    return RedirectToAction("LogIn", "Account", new { area = "" });
+                    return RedirectToAction("LogIn", "Account", new { returnUrl = "Panier" });
                 }else{
                 return View();
             }
+        }
+        public ActionResult AjoutPanier(int Code)
+        {
+            if (!Request.IsAuthenticated)
+            {
+                return RedirectToAction("LogIn", "Account", new { returnUrl = "" });
+            }
+            else
+            {
+                if (Session["Panier"] == null)
+                {
+                    Session["Panier"] = new int[] { Code };
+                }
+                else {
+                    int[] a = (int[])Session["Panier"];
+                    a.ToList().Add(Code);
+                    Session["Panier"] = a;
+                }
+                return RedirectToAction("", "Home", new { });
+            }
+            
         }
     }
 }
